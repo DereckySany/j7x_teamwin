e57d95ffeac0357c6bc3c25840b75e94ebf7505e
yEsPaP <46491239+yEsPaP@users.noreply.github.com>
2019-05-12 19:51:43 +0300

    Init tree j7x

---
+++ omni_j7xelte.mk
@@ -0,0 +1,16 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) 
+
+# Inherit some common omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_PACKAGES += \
+	charger_res_images \
+	charger
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_j7xelte
+PRODUCT_RELEASE_NAME := j7xelte
+PRODUCT_DEVICE := j7xelte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
